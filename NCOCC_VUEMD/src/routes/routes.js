0cd0e6de02d659ff1605bf34927d4f021c46365b
William.D.Joshua <yourenlaila@hotmail.com>
2019-03-03 19:53:23 +0800

    add Vue Material Dashboard UI

---
+++ NCOCC_VUEMD/src/routes/routes.js
@@ -0,0 +1,71 @@
+import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
+
+import Dashboard from "@/pages/Dashboard.vue";
+import UserProfile from "@/pages/UserProfile.vue";
+import TableList from "@/pages/TableList.vue";
+import Typography from "@/pages/Typography.vue";
+import Icons from "@/pages/Icons.vue";
+import Maps from "@/pages/Maps.vue";
+import Notifications from "@/pages/Notifications.vue";
+import UpgradeToPRO from "@/pages/UpgradeToPRO.vue";
+import WaybillList from "@/pages/WaybillCenter/WaybillList.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "首页",
+        component: Dashboard
+      },
+      {
+        path: "user",
+        name: "用户资料",
+        component: UserProfile
+      },
+      {
+        path: "table",
+        name: "Table List",
+        component: TableList
+      },
+      {
+        path: "waybill",
+        name: "Waybill Center",
+        component: WaybillList
+      },
+      {
+        path: "typography",
+        name: "Typography",
+        component: Typography
+      },
+      {
+        path: "icons",
+        name: "Icons",
+        component: Icons
+      },
+      {
+        path: "maps",
+        name: "Maps",
+        meta: {
+          hideFooter: true
+        },
+        component: Maps
+      },
+      {
+        path: "notifications",
+        name: "Notifications",
+        component: Notifications
+      },
+      {
+        path: "upgrade",
+        name: "Upgrade to PRO",
+        component: UpgradeToPRO
+      }
+    ]
+  }
+];
+
+export default routes;
